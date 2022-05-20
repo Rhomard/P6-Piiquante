1e3ac2b1806634bdc44c2ec8bf476a5ca7d0fcc8
Rhomard <romhaar@gmail.com>
2022-05-20 18:19:25 +0200

    ignore images folder

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -11,10 +11,6 @@ exports.createSauce = (req, res, next) => {
       const sauce = new Sauce({
             ...sauceObject,
             imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
-            likes: 0,
-            dislikes: 0,
-            usersLiked: [],
-            usersDisliked: [],
       });
       sauce.save()
             .then(() => res.status(201).json({ message: "Objet enregistré !" }))
